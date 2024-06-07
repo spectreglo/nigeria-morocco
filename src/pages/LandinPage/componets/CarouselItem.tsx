542187798f99dcb1109adfffa1fe4ad457b8b032
Abdulhafiz Bindawa <abdulhafizbdw@gmail.com>
2024-06-07 07:42:47 +0100

    background image cover

--- src/pages/LandinPage/componets/CarouselItem.tsx
+++ src/pages/LandinPage/componets/CarouselItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface ItemType {
   left: React.ReactNode;
@@ -16,10 +16,12 @@ export default function CarouselItem({ item, ind }: CarouselItemProps) {
       style={{
         background:
           ind > 0
-            ? `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(hero${ind}.png)`
-            : 'rgb(245,247,250)',
+            ? `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(hero${ind}.png) no-repeat fixed center`
+            : "rgb(245,247,250)",
+        backgroundSize: "cover",
       }}
-      className="flex w-full px-5 flex-col min-h-[50vh] md:min-h-[80vh] justify-start md:flex-row md:justify-between items-center flex-1 bg-silver md:px-20 lg:px-40">
+      className="flex w-full px-5 flex-col min-h-[50vh] md:min-h-[80vh] justify-start md:flex-row md:justify-between items-center flex-1 bg-silver md:px-20 lg:px-40"
+    >
       {item.left}
       {item.right}
     </div>
