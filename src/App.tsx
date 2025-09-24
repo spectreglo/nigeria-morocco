d4a021848664dd31ac5b349a0afc29a63e50a28c
Abdulhafiz Bindawa <abdulhafizbdw@gmail.com>
2025-09-24 22:43:56 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LandinPage from './pages/LandinPage/LandinPage';
-import { ConfigProvider } from 'antd';
-import Register from './pages/Forms/Register';
-import Invoice from './pages/Invoice/Invoice';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandinPage from "./pages/LandinPage/LandinPage";
+import { ConfigProvider } from "antd";
+// import Register from "./pages/Forms/Register";
+// import Invoice from "./pages/Invoice/Invoice";
 
-import DashboardHome from './pages/Dashboard/DashboardHome';
-import { Suspense } from 'react';
-import Booking from './pages/Forms/Booking';
-import BookingInvoice from './pages/Invoice/BookingInvoice';
-import Login from './pages/Auth/Login';
-import PrivateRoutes from './pages/PrivateRoutes';
-import { Provider } from 'react-redux';
-import store from './redux';
-import Profile from './pages/Dashboard/profile/Profile';
-import UserProfile from './pages/Dashboard/profile/UserProfile';
-import Users from './pages/Users/Users';
-import Success from './pages/Forms/Success';
-import Bookings from './pages/Bookings/Bookings';
+// import DashboardHome from "./pages/Dashboard/DashboardHome";
+import { Suspense } from "react";
+// import Booking from "./pages/Forms/Booking";
+// import BookingInvoice from "./pages/Invoice/BookingInvoice";
+// import Login from "./pages/Auth/Login";
+// import PrivateRoutes from "./pages/PrivateRoutes";
+import { Provider } from "react-redux";
+import store from "./redux";
+// import Profile from "./pages/Dashboard/profile/Profile";
+// import UserProfile from "./pages/Dashboard/profile/UserProfile";
+// import Users from "./pages/Users/Users";
+// import Success from "./pages/Forms/Success";
+// import Bookings from "./pages/Bookings/Bookings";
 // MADE IT PUBLIC
 
 function App() {
@@ -29,15 +29,16 @@ function App() {
             theme={{
               token: {
                 // Seed Token
-                colorPrimary: '#4CAF4F',
+                colorPrimary: "#4CAF4F",
 
                 // Alias Token
-                colorBgContainer: '#f6ffed',
+                colorBgContainer: "#f6ffed",
               },
-            }}>
+            }}
+          >
             <Routes>
               <Route path="/" element={<LandinPage />} />
-              <Route path="/Register" element={<Register />} />
+              {/* <Route path="/Register" element={<Register />} />
               <Route path="/Booking" element={<Booking />} />
               <Route path="/Invoice" element={<Invoice />} />
               <Route path="/BookingInvoice" element={<BookingInvoice />} />
@@ -49,7 +50,7 @@ function App() {
                 <Route path="profile" element={<Profile />} />
                 <Route path="users" element={<Users />} />
                 <Route path="bookings" element={<Bookings />} />
-              </Route>
+              </Route> */}
             </Routes>
           </ConfigProvider>
         </Provider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import 'react-phone-number-input/style.css';
-import './i18n';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "react-phone-number-input/style.css";
+import "./i18n";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    kdkddkkk
   </React.StrictMode>
 );
