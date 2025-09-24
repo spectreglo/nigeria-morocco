f8539292f00ff0123176b9114421d59b352faf52
Abdulhafiz Bindawa <abdulhafizbdw@gmail.com>
2025-09-24 23:14:22 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandinPage from "./pages/LandinPage/LandinPage";
+import { ConfigProvider } from "antd";
+import Register from "./pages/Forms/Register";
+import Invoice from "./pages/Invoice/Invoice";
 
-import { ConfigProvider } from 'antd';
-
-import { Suspense } from 'react';
-
-import { Provider } from 'react-redux';
-import store from './redux';
-
-import NewLanding from './pages/LandinPage/NewLanding';
+import DashboardHome from "./pages/Dashboard/DashboardHome";
+import { Suspense } from "react";
+import Booking from "./pages/Forms/Booking";
+import BookingInvoice from "./pages/Invoice/BookingInvoice";
+import Login from "./pages/Auth/Login";
+import PrivateRoutes from "./pages/PrivateRoutes";
+import { Provider } from "react-redux";
+import store from "./redux";
+import Profile from "./pages/Dashboard/profile/Profile";
+import UserProfile from "./pages/Dashboard/profile/UserProfile";
+import Users from "./pages/Users/Users";
+import Success from "./pages/Forms/Success";
+import Bookings from "./pages/Bookings/Bookings";
+// MADE IT PUBLIC
 
 function App() {
   return (
@@ -19,19 +29,20 @@ function App() {
             theme={{
               token: {
                 // Seed Token
-                colorPrimary: '#4CAF4F',
+                colorPrimary: "#4CAF4F",
 
                 // Alias Token
-                colorBgContainer: '#f6ffed',
+                colorBgContainer: "#f6ffed",
               },
-            }}>
+            }}
+          >
             <Routes>
-              <Route path="/" element={<NewLanding />} />
-              {/* <Route path="/Register" element={<Register />} />
+              <Route path="/" element={<LandinPage />} />
+              <Route path="/Register" element={<Register />} />
               <Route path="/Booking" element={<Booking />} />
-              <Route path="/Invoice" element={<Invoice />} />
-              <Route path="/BookingInvoice" element={<BookingInvoice />} />
-              <Route path="/Success" element={<Success />} />
+              {/* <Route path="/Invoice" element={<Invoice />} /> */}
+              {/* <Route path="/BookingInvoice" element={<BookingInvoice />} /> */}
+              {/* <Route path="/Success" element={<Success />} /> */}
               <Route path="/Login" element={<Login />} />
               <Route path="/dashboard" element={<PrivateRoutes />}>
                 <Route index element={<DashboardHome />} />
@@ -39,7 +50,7 @@ function App() {
                 <Route path="profile" element={<Profile />} />
                 <Route path="users" element={<Users />} />
                 <Route path="bookings" element={<Bookings />} />
-              </Route> */}
+              </Route>
             </Routes>
           </ConfigProvider>
         </Provider>
